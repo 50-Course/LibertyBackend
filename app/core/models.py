a1f1be62d2a12d365b06bafa767ac078e9892790
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-22 18:27:41 +0100

    feat[Product Management - Setup table schema, database Migrations]

--- app/core/models.py
+++ app/core/models.py
@@ -44,3 +44,45 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Product(models.Model):
+    """
+    Item sold at this e-mart
+
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(help_text="Description of the item")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        # Here we want to consider a sceneraio where our store, has thousands
+        # or lots of products, and we need to work with large dataset.
+        # We might need some form of optimization by tehn.
+        #
+        # Here we are indexing the name of the product, and the price tag
+        # as well as adding, we might also want ot add a UniqueConstriant,
+        # A product --> should have a fixed price tag, but that would happen
+        # in an ideal world right
+        indexes = [
+            models.Index(fields=["name", "price"], name="name_price_idx"),
+        ]
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Product category, e.g groceries, scents, gadgets",
+        db_index=True,
+    )  # For product categories, we only want to index the category name to help us in faster data retrieval
+    # now in small datasets/querysets, we don't need this optimization, but here just want to drive home a point
+    product = models.ForeignKey(
+        Product, related_name="product_category", on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.name
